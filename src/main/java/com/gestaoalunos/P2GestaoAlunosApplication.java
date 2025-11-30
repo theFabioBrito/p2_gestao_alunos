03bf4606671f5f8377bf0d9e752e7f7001661965
Fabio <fabiosb.05@gmail.com>
2025-11-30 20:27:36 -0300

    Primeiro commit no projeto da P2 da LP3

---
+++ src/main/java/com/gestaoalunos/P2GestaoAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoalunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2GestaoAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2GestaoAlunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaoalunos/P2GestaoAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaoalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2GestaoAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
